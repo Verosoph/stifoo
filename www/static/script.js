2788ff1f37ee1762f605f908a14fda75654acbb9
Marco Mettke <mettkemarco@gmail.com>
2018-03-20 22:40:26 +0100

    final

--- www/static/script.js
+++ www/static/script.js
@@ -1,37 +1,10 @@
-var show_content = function(destination){
-    $.ajax({
-        url: 'content/'+destination +'.html', 
-        success: function(response) {
-            $('.content').html(response);
-        }
-    })
-};
+var W = new Array('So','Mo','Di','Mi','Do','Fr','Sa','So'),H=new Date(),
+E = new Array("Januar","Februar","März","April","Mai","Juni",
+"Juli","August","September","Oktober","November","Dezember");
 
+var date= (W[H.getDay()]+". "+H.getDate()+". "+E[H.getMonth()]+" "+H.getFullYear());
 
-// var W = new Array('So','Mo','Di','Mi','Do','Fr','Sa','So'),H=new Date(),
-// E = new Array("Januar","Februar","März","April","Mai","Juni",
-// "Juli","August","September","Oktober","November","Dezember");
-
-// //(W[H.getDay()]+"., "+H.getDate()+". "+E[H.getMonth()]+" "+H.getFullYear()
-
-
-
-// function somefunction(){
-//     var date = "date";
-//     $(".date").html(date);
-//   }
-
-// somefunction();
 
 $(document).ready(function(){
-    show_content('home');
-    $('#navi li a').on('click', function(){
-        var dest = $(this).data('id');
-        show_content(dest); 
-    });
-    
-  
-
-
-
+    $(".date").html(date);
 });
